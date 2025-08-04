1857b4d74c4a5d0b592077a5e618077e27efdb31
Haochen Shen <haochenshen@mac.watson.ibm.com>
2025-08-04 10:01:40 -0400

    Continue debugging

--- vllm/model_executor/models/zamba2.py
+++ vllm/model_executor/models/zamba2.py
@@ -773,6 +773,47 @@ class Zamba2Model(nn.Module):
         hidden_states = self.final_layernorm(hidden_states)
         return hidden_states
 
+    def load_weights(self, weights: Iterable[tuple[str,
+                                                   torch.Tensor]]) -> set[str]:
+        stacked_params_mapping = [
+            # (param_name, shard_name, shard_id)
+            ("qkv_proj", "q_proj", "q"),
+            ("qkv_proj", "k_proj", "k"),
+            ("qkv_proj", "v_proj", "v"),
+        ]
+
+        params_dict = dict(self.named_parameters())
+        loaded_params: set[str] = set()
+        for chkpt_weight_name, loaded_weight in weights:
+            for param_name, weight_name, shard_id in stacked_params_mapping:
+                if weight_name not in chkpt_weight_name:
+                    continue
+                chkpt_weight_name = chkpt_weight_name.replace(
+                    weight_name, param_name)
+                param = params_dict[chkpt_weight_name]
+                weight_loader = param.weight_loader
+                weight_loader(param, loaded_weight, shard_id)
+                break
+            else:
+                if chkpt_weight_name not in params_dict:
+                    continue
+                param = params_dict[chkpt_weight_name]
+                weight_loader = getattr(param, "weight_loader",
+                                        default_weight_loader)
+                weight_loader(param, loaded_weight)
+            loaded_params.add(chkpt_weight_name)
+        
+        # If upi_mask is not in the checkpoint, use the dafault values instead
+        for layer_idx, layer_type in enumerate(self.config.layers_block_type):
+            if layer_type == "hybrid":
+                loaded_params.add(f'model.layers.{layer_idx}.mamba_decoder.mamba.upi_mask')
+                print(f"Loaded model.layers.{layer_idx}.mamba_decoder.mamba.upi_mask")
+            else:
+                loaded_params.add(f'model.layers.{layer_idx}.mamba.upi_mask')
+                print(f"Loaded model.layers.{layer_idx}.mamba.upi_mask")
+
+        return loaded_params
+
 
 class Zamba2ForCausalLM(nn.Module, HasInnerState, IsHybrid):
     """Zamba2 model with causal language modeling head.
@@ -986,34 +1027,9 @@ class Zamba2ForCausalLM(nn.Module, HasInnerState, IsHybrid):
 
     def load_weights(self, weights: Iterable[tuple[str,
                                                    torch.Tensor]]) -> set[str]:
-        stacked_params_mapping = [
-            # (param_name, shard_name, shard_id)
-            ("qkv_proj", "q_proj", "q"),
-            ("qkv_proj", "k_proj", "k"),
-            ("qkv_proj", "v_proj", "v"),
-        ]
-
-        params_dict = dict(self.named_parameters())
-        loaded_params: set[str] = set()
-        for chkpt_weight_name, loaded_weight in weights:
-            for param_name, weight_name, shard_id in stacked_params_mapping:
-                if weight_name not in chkpt_weight_name:
-                    continue
-                chkpt_weight_name = chkpt_weight_name.replace(
-                    weight_name, param_name)
-                param = params_dict[chkpt_weight_name]
-                weight_loader = param.weight_loader
-                weight_loader(param, loaded_weight, shard_id)
-                break
-            else:
-                if chkpt_weight_name not in params_dict:
-                    continue
-                param = params_dict[chkpt_weight_name]
-                weight_loader = getattr(param, "weight_loader",
-                                        default_weight_loader)
-                weight_loader(param, loaded_weight)
-            loaded_params.add(chkpt_weight_name)
-        
+        loader = AutoWeightsLoader(self)
+        loaded_params = loader.load_weights(weights, mapper=self.hf_to_vllm_mapper)
+    
         # If upi_mask is not in the checkpoint, use the dafault values instead
         for layer_idx, layer_type in enumerate(self.config.layers_block_type):
             if layer_type == "hybrid":
@@ -1022,5 +1038,5 @@ class Zamba2ForCausalLM(nn.Module, HasInnerState, IsHybrid):
             else:
                 loaded_params.add(f'model.layers.{layer_idx}.mamba.upi_mask')
                 print(f"Loaded model.layers.{layer_idx}.mamba.upi_mask")
-
+        
         return loaded_params
